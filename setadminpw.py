b0a05679414e87f1166d38b9b8b7ff23172d2ef3
Harlin Seritt <harlin.seritt@gmail.com>
2020-02-26 21:57:07 -0700

    Add redirect from / to docweb/

--- setadminpw.py
+++ setadminpw.py
@@ -11,6 +11,9 @@ from django.contrib.auth.models import User  # noqa E402
 
 
 if __name__ == '__main__':
+    print("  Setting admin user password ...")
     admin_user = User.objects.get(username='admin')
     admin_user.set_password('admin')
     admin_user.save()
+    print("  Done.")
+
