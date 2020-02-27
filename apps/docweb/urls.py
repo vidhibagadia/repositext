fc5fac7d3a05abb454e1b133c31fa5b9e4e4dfa0
Vidhi Bagadia <vidbagadia@gmail.com>
2020-02-27 23:15:38 +0530

    Use url tags for href

--- apps/docweb/urls.py
+++ apps/docweb/urls.py
@@ -3,7 +3,7 @@ from .views import IndexView, RepositoryView
 
 
 urlpatterns = [
-    path('', IndexView.as_view()),
-    path('repository/folder/-ROOT-/', RepositoryView.as_view()),
-    path('repository/folder/<uuid:folder_id>/', RepositoryView.as_view()),
+    path('', IndexView.as_view(), name="index-view"),
+    path('repository/folder/-ROOT-/', RepositoryView.as_view(), name="root-view"),
+    path('repository/folder/<uuid:folder_id>/', RepositoryView.as_view(), name="repo-view"),
 ]
